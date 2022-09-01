e6237cd2cffd37e91c9f8f7cde01fa6850a864bf
sparshg <43041139+sparshg@users.noreply.github.com>
2022-09-01 22:30:05 +0530

    day 03

--- src/day03.rs
+++ src/day03.rs
@@ -36,20 +36,46 @@ fn part1(input: &str) -> i32 {
             }
         }
     }
-
-    // let a = get_overlaps(P(0, 0), P(4, 4), P(2, 4), P(6, 6));
-    // if a.is_some() {
-    //     a.unwrap().
-    // }
     overlaps.len() as i32
 }
 
 fn part2(input: &str) -> i32 {
+    let input = input
+        .lines()
+        .map(|x| {
+            x.split(|x| !char::is_numeric(x))
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect_vec()
+        })
+        .collect_vec();
+
+    for (i, r) in input.iter().enumerate() {
+        let mut answer = true;
+        for (j, o) in input.iter().enumerate() {
+            if i != j {
+                if get_overlaps(
+                    &P(r[1], r[2]),
+                    &P(r[1] + r[3] - 1, r[2] + r[4] - 1),
+                    &P(o[1], o[2]),
+                    &P(o[1] + o[3] - 1, o[2] + o[4] - 1),
+                )
+                .is_some()
+                {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        if answer {
+            return r[0];
+        }
+    }
     0
 }
 
 fn get_overlaps(p1: &P, p2: &P, q1: &P, q2: &P) -> Option<(P, P)> {
-    if (q1.1 < p2.1 && q2.1 > p1.1) && (q1.0 < p2.0 && q2.0 > p1.0) {
+    if (q1.1 <= p2.1 && q2.1 >= p1.1) && (q1.0 <= p2.0 && q2.0 >= p1.0) {
         return Some((
             P(max(q1.0, p1.0), max(q1.1, p1.1)),
             P(min(q2.0, p2.0), min(q2.1, p2.1)),
@@ -60,7 +86,7 @@ fn get_overlaps(p1: &P, p2: &P, q1: &P, q2: &P) -> Option<(P, P)> {
 
 #[allow(unreachable_code)]
 pub fn run(input: &str) -> Option<i32> {
-    return Some(part1(input));
-    // return Some(part2(input));
+    // return Some(part1(input));
+    return Some(part2(input));
     None
 }
