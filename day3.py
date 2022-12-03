dcbf5cc09f454bed645e7bdcd0cd135a0190bb8d
sparshg <43041139+sparshg@users.noreply.github.com>
2022-12-03 23:31:56 +0530

    remove readlines()

--- day3.py
+++ day3.py
@@ -1,11 +1,11 @@
 s = 0
-for line in open('input.txt', 'r').readlines():
+for line in open('input.txt', 'r'):
     first, second = line[:len(line)//2], line[len(line)//2:]
     s += [ord(c) - 96 if ord(c) > 96 else ord(c) - 38 for c in first if c in second][0]
 print(s)
 
 s = 0
-for i, line in enumerate(open('input.txt', 'r').readlines()):
+for i, line in enumerate(open('input.txt', 'r')):
     if i % 3 == 0:
         first = line
     elif i % 3 == 1:
