b009fda810f91ec230f51bab0db07ef4664b3454
sparshg <43041139+sparshg@users.noreply.github.com>
2023-12-08 20:31:41 +0530

    day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,14 +1,37 @@
+use std::collections::HashMap;
+
 fn part1(input: &str) -> i32 {
-    0
+    let colors = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
+    input.lines().enumerate().fold(0, |sum, (i, line)| {
+        sum + if line.split_once(':').unwrap().1.split(&[';', ',']).any(|x| {
+            let (n, c) = x.trim().split_once(' ').unwrap();
+            n.parse::<i32>().unwrap() > colors[c]
+        }) {
+            0
+        } else {
+            i as i32 + 1
+        }
+    })
 }
 
 fn part2(input: &str) -> i32 {
-    0
+    input.lines().fold(0, |sum, line| {
+        let mut colors = HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
+        line.split_once(':')
+            .unwrap()
+            .1
+            .split(&[';', ','])
+            .for_each(|x| {
+                let (n, c) = x.trim().split_once(' ').unwrap();
+                colors.insert(c, n.parse::<i32>().unwrap().max(colors[c]));
+            });
+        sum + colors.values().fold(1, |p, x| p * x)
+    })
 }
 
 #[allow(unreachable_code)]
 pub fn run(input: &str) -> Option<i32> {
     // return Some(part1(input));
-    // return Some(part2(input));
+    return Some(part2(input));
     None
 }
