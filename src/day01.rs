e85c53e0e2e2f0d66f1f47f84842499f15145e87
sparshg <43041139+sparshg@users.noreply.github.com>
2023-12-08 14:35:23 +0530

    2023: day 1 part 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,35 +1,33 @@
+use std::i32;
+
+
+
 fn part1(input: &str) -> i32 {
-    input
-        .lines()
-        .fold(0, |sum, line| sum + line.parse::<i32>().unwrap())
+    input.lines().fold(0, |sum, line| {
+        sum + line
+            .chars()
+            .find(|&x| x.is_numeric())
+            .unwrap()
+            .to_digit(10)
+            .unwrap() as i32
+            * 10
+            + line
+                .chars()
+                .rev()
+                .find(|&x| x.is_numeric())
+                .unwrap()
+                .to_digit(10)
+                .unwrap() as i32
+    })
 }
 
 fn part2(input: &str) -> i32 {
-    let input: Vec<i32> = input.lines().map(|x| x.parse().unwrap()).collect();
-    let mut sums: Vec<i32> = vec![0; input.len()];
-    for a in 0..input.len() {
-        for b in a..input.len() {
-            sums[a] += input[b];
-            if sums[a] == 0 {
-                return sums[0];
-            }
-        }
-    }
-    loop {
-        for a in &input {
-            for x in 0..sums.len() {
-                sums[x] += a;
-                if sums[x] == 0 {
-                    return sums[0];
-                }
-            }
-        }
-    }
+    0
 }
 
 #[allow(unreachable_code)]
 pub fn run(input: &str) -> Option<i32> {
-    // return Some(part1(input));
+    return Some(part1(input));
     // return Some(part2(input));
     None
 }
--- src/day02.rs
+++ src/day02.rs
@@ -1,40 +1,8 @@
-use itertools::Itertools;
-
 fn part1(input: &str) -> i32 {
-    input
-        .lines()
-        .fold(vec![0, 0], |mut acc, curr| {
-            let mut duplicates = curr.chars().sorted().dedup_with_count();
-            acc[0] += duplicates.clone().any(|(x, _)| x == 2) as i32;
-            acc[1] += duplicates.any(|(x, _)| x == 3) as i32;
-            acc
-        })
-        .iter()
-        .product()
+    0
 }
 
 fn part2(input: &str) -> i32 {
-    for i in 0..input.lines().count() {
-        let mut it = input.lines().skip(i);
-        let curr = it.next().unwrap();
-        for x in it {
-            if curr
-                .chars()
-                .zip(x.chars())
-                .fold(0, |acc, (x, y)| if x != y { acc + 1 } else { acc })
-                == 1
-            {
-                println!(
-                    "{}",
-                    curr.chars()
-                        .zip(x.chars())
-                        .filter_map(|(x, y)| if x == y { Some(x) } else { None })
-                        .collect::<String>()
-                );
-                return 0;
-            }
-        }
-    }
     0
 }
 
