38099c9ff96c3fb5a521a925cba8ab7e4c18ca0f
sparshg <43041139+sparshg@users.noreply.github.com>
2023-12-21 14:15:01 +0530

    day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,14 +1,39 @@
+use itertools::Itertools;
+
 fn part1(input: &str) -> i32 {
-    0
+    let input = input
+        .lines()
+        .take(2)
+        .map(|l| {
+            l.split_once(':')
+                .unwrap()
+                .1
+                .split_whitespace()
+                .map(|y| y.parse::<f64>().unwrap())
+                .collect_vec()
+        })
+        .collect_tuple::<(Vec<f64>, Vec<f64>)>()
+        .unwrap();
+    input
+        .0
+        .into_iter()
+        .zip(input.1)
+        .map(|(n, k)| {
+            (((n + (n * n - 4. * k).sqrt()) * 0.5 - 1.).ceil()
+                - ((n - (n * n - 4. * k).sqrt()) * 0.5 + 1.).floor()) as i32
+                + 1
+        })
+        .product()
 }
 
 fn part2(input: &str) -> i32 {
+    // modify input manually, run part 1
     0
 }
 
 #[allow(unreachable_code)]
 pub fn run(input: &str) -> Option<i32> {
-    // return Some(part1(input));
+    return Some(part1(input));
     // return Some(part2(input));
     None
 }
