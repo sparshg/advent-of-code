65084c5884e0de72538c9666f15709bcc49c872e
sparshg <43041139+sparshg@users.noreply.github.com>
2024-12-01 14:18:38 +0530

    day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,59 +1,37 @@
 use std::{collections::HashMap, i32};
 
+use itertools::Itertools;
+
+fn input_vec(input: &str) -> (Vec<i32>, Vec<i32>) {
+    input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(a, b)| (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()))
+        .unzip()
+}
+
 fn part1(input: &str) -> i32 {
-    input.lines().fold(0, |sum, line| {
-        sum + line
-            .chars()
-            .find(|&x| x.is_numeric())
-            .unwrap()
-            .to_digit(10)
-            .unwrap() as i32
-            * 10
-            + line
-                .chars()
-                .rev()
-                .find(|&x| x.is_numeric())
-                .unwrap()
-                .to_digit(10)
-                .unwrap() as i32
-    })
+    let (a, b) = input_vec(input);
+
+    a.into_iter()
+        .sorted()
+        .zip(b.into_iter().sorted())
+        .map(|(a, b)| (a - b).abs())
+        .sum()
 }
 
 fn part2(input: &str) -> i32 {
-    let m = HashMap::from([
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-        ("zero", 0),
-    ]);
-    input.lines().fold(0, |sum, line: &str| {
-        let (mut first, mut last) = (None, None);
-        for (i, c) in line.chars().enumerate() {
-            if let Some(num) = m
-                .get(&line.get(i..i + 3).unwrap_or_default())
-                .or(m.get(&line.get(i..i + 4).unwrap_or_default()))
-                .or(m.get(&line.get(i..i + 5).unwrap_or_default())).copied()
-                .or(c.to_digit(10))
-            {
-                if first.is_none() {
-                    first = Some(num);
-                }
-                last = Some(num);
-            }
-        }
-        sum + (first.unwrap() * 10 + last.unwrap()) as i32
-    })
+    let (a, b) = input_vec(input);
+    let b = b.into_iter().counts();
+
+    a.into_iter()
+        .map(|x| x * *b.get(&x).unwrap_or(&0) as i32)
+        .sum()
 }
 
 #[allow(unreachable_code)]
 pub fn run(input: &str) -> Option<i32> {
     // return Some(part1(input));
-    // return Some(part2(input));
+    return Some(part2(input));
     None
 }
