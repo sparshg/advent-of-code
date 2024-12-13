732e401b1b8e7b79b209748ab94f500555813086
sparshg <43041139+sparshg@users.noreply.github.com>
2024-12-13 22:33:43 +0530

    day 11 part 1

--- src/day11.rs
+++ src/day11.rs
@@ -1,5 +1,30 @@
+use itertools::Itertools;
+
 fn part1(input: &str) -> i32 {
-    0
+    let mut nums = input
+        .split_whitespace()
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect_vec();
+    for i in 0..75 {
+        let mut ext = Vec::new();
+        for x in nums.iter_mut() {
+            if x == &0 {
+                *x = 1;
+                continue;
+            }
+            let digits = x.ilog10() + 1;
+            if digits & 1 == 0 {
+                let mask = 10u64.pow(digits / 2);
+                ext.push(*x % mask);
+                *x /= mask;
+            } else {
+                *x *= 2024;
+            }
+        }
+        nums.extend(ext);
+        dbg!(i, nums.iter().count());
+    }
+    nums.iter().count() as i32
 }
 
 fn part2(input: &str) -> i32 {
