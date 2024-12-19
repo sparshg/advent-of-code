4525dc9da1a014d521e71c9f67126dc0998a3c92
sparshg <43041139+sparshg@users.noreply.github.com>
2024-12-19 14:12:38 +0530

    day 19

--- src/day19.rs
+++ src/day19.rs
@@ -1,8 +1,36 @@
+use itertools::Itertools;
+
+fn backtrack(i: usize, towels: &[&str], pattern: &str, dp: &mut Vec<Option<u64>>) -> u64 {
+    if i >= pattern.len() {
+        return 1;
+    }
+    if let Some(res) = dp[i] {
+        return res;
+    }
+    let mut ways = 0;
+    for &towel in towels {
+        if i + towel.len() <= pattern.len() && *towel == pattern[i..i + towel.len()] {
+            ways += backtrack(i + towel.len(), towels, pattern, dp);
+        }
+    }
+    dp[i] = Some(ways);
+    ways
+}
+
+fn ways(input: &str) -> impl Iterator<Item = u64> + '_ {
+    let towels = input.lines().next().unwrap().split(", ").collect_vec();
+    input
+        .lines()
+        .skip(2)
+        .map(move |line| backtrack(0, &towels, line, &mut vec![None; line.len()]))
+}
+
 fn part1(input: &str) -> i32 {
-    0
+    ways(input).filter(|&x| x > 0).count() as i32
 }
 
 fn part2(input: &str) -> i32 {
+    println!("{}", ways(input).sum::<u64>());
     0
 }
 
