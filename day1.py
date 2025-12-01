11e85432930634f0000890401bb478508daba9e7
sparshg <43041139+sparshg@users.noreply.github.com>
2025-12-01 19:46:43 +0530

    day 1

--- day1.py
+++ day1.py
@@ -1,4 +1,10 @@
-inp = open('input.txt', 'r').read().strip().split('\n\n')
-inp = list(map(lambda i: sum([int(x) for x in i.split('\n')]), inp))
-print(max(inp))
-print(sum(sorted(inp, reverse=True)[:3]))
+inp = open('input.txt', 'r').read().strip().split('\n')
+ptr, res1, res2 = 50, 0, 0
+
+for op, num in map(lambda x: (x[0], int(x[1:])), inp):
+    ptr = 100 if ptr == 0 and op == 'L' else ptr
+    ptr = (ptr - num) if op == 'L' else (ptr + num)
+    res2 += -((ptr - 1) // 100) if op == 'L' else ptr // 100
+    ptr %= 100
+    res1 += ptr == 0
+print(res1, res2)
